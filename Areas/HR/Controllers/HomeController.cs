7c34a21820e1504d5dcefddba7e3852c44e5cd0f
BinhTeach <2324801030003@student.tdmu.edu.vn>
2025-12-04 16:10:48 +0700

    dashboard HR

--- Areas/HR/Controllers/HomeController.cs
+++ Areas/HR/Controllers/HomeController.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using QLNSVATC.Helper;
+using QLNSVATC.Helpers;
 using QLNSVATC.Models;
-
 namespace QLNSVATC.Areas.HR.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: HR/Home
+
+        private readonly QLNSVATCEntities db = new QLNSVATCEntities();
+
         public ActionResult Index()
         {
             if (!CheckAccess.Role("HR"))
@@ -19,8 +21,135 @@ namespace QLNSVATC.Areas.HR.Controllers
                 return RedirectToAction("Login", "Account", new { area = "" });
             }
 
-            return View();
-        }
+            var model = new HRDashboardViewModel();
 
+            string userId = Session["UserId"] as string;
+            UserViewBagModel stc = SettingsHelper.BuildViewBagData(db, userId);
+            ViewBag.Settings = stc;  
+
+            model.TotalEmployees = db.NHANVIENs.Count();
+            model.TotalDepartments = db.PHONGBANs.Count();
+
+            model.TotalMale = db.NHANVIENs.Count(x => x.GIOITINH == true);
+            model.TotalFemale = db.NHANVIENs.Count(x => x.GIOITINH == false);
+
+            model.EmployeesWithInsurance = db.THONGTINBAOHIEMs
+                .Select(x => x.MANV)
+                .Distinct()
+                .Count();
+
+            model.EmployeesWithHealthInfo = db.THONGTINSUCKHOEs
+                .Select(x => x.MANV)
+                .Distinct()
+                .Count();
+
+            model.TopSalaries = (from nv in db.NHANVIENs
+                                 join l in db.LUONGs on nv.MANV equals l.MANV
+                                 join pb in db.PHONGBANs on nv.MAPB equals pb.MAPB into gpb
+                                 from pb in gpb.DefaultIfEmpty()
+                                 orderby l.LUONGCOBAN descending
+                                 select new EmployeeRankingItem
+                                 {
+                                     MaNV = nv.MANV,
+                                     HoTen = nv.HOLOT + " " + nv.TENNV,
+                                     TenPhongBan = pb != null ? pb.TENPB : "(Chưa gán)",
+                                     Value = l.LUONGCOBAN ?? 0,
+                                     Unit = "VND",
+                                     Label = "Lương cơ bản"
+                                 })
+                                 .Take(5)
+                                 .ToList();
+
+            model.TopBonuses = (from tp in db.DSTHUONGPHATs
+                                where tp.HINHTHUC == "KT"      
+                                group tp by tp.MANV into g
+                                join nv in db.NHANVIENs on g.Key equals nv.MANV
+                                join pb in db.PHONGBANs on nv.MAPB equals pb.MAPB into gpb
+                                from pb in gpb.DefaultIfEmpty()
+                                orderby g.Sum(x => x.TONG) descending
+                                select new EmployeeRankingItem
+                                {
+                                    MaNV = nv.MANV,
+                                    HoTen = nv.HOLOT + " " + nv.TENNV,
+                                    TenPhongBan = pb != null ? pb.TENPB : "(Chưa gán)",
+                                    Value = g.Sum(x => x.TONG) ?? 0,
+                                    Unit = "VND",
+                                    Label = "Tổng thưởng"
+                                })
+                                .Take(5)
+                                .ToList();
+
+            model.TopPenalties = (from tp in db.DSTHUONGPHATs
+                                  where tp.HINHTHUC == "KL"   
+                                  group tp by tp.MANV into g
+                                  join nv in db.NHANVIENs on g.Key equals nv.MANV
+                                  join pb in db.PHONGBANs on nv.MAPB equals pb.MAPB into gpb
+                                  from pb in gpb.DefaultIfEmpty()
+                                  orderby g.Sum(x => x.TONG) descending
+                                  select new EmployeeRankingItem
+                                  {
+                                      MaNV = nv.MANV,
+                                      HoTen = nv.HOLOT + " " + nv.TENNV,
+                                      TenPhongBan = pb != null ? pb.TENPB : "(Chưa gán)",
+                                      Value = g.Sum(x => x.TONG) ?? 0,
+                                      Unit = "VND",
+                                      Label = "Tổng phạt"
+                                  })
+                                  .Take(5)
+                                  .ToList();
+
+            model.DepartmentSummaries = (from pb in db.PHONGBANs
+                                         join nv in db.NHANVIENs on pb.MAPB equals nv.MAPB into nvs
+                                         select new DepartmentSummaryItem
+                                         {
+                                             MaPB = pb.MAPB,
+                                             TenPB = pb.TENPB,
+                                             SoNhanVien = nvs.Count(),
+                                             SoNam = nvs.Count(x => x.GIOITINH == true),
+                                             SoNu = nvs.Count(x => x.GIOITINH == false)
+                                         })
+                                         .OrderByDescending(x => x.SoNhanVien)
+                                         .ToList();
+
+            model.TopOvertime = (from cc in db.CHAMCONGs
+                                 group cc by cc.MANV into g
+                                 join nv in db.NHANVIENs on g.Key equals nv.MANV
+                                 join pb in db.PHONGBANs on nv.MAPB equals pb.MAPB into gpb
+                                 from pb in gpb.DefaultIfEmpty()
+                                 orderby g.Sum(x => x.TONGCA) descending
+                                 select new AttendanceSummaryItem
+                                 {
+                                     MaNV = nv.MANV,
+                                     HoTen = nv.HOLOT + " " + nv.TENNV,
+                                     TenPhongBan = pb != null ? pb.TENPB : "(Chưa gán)",
+                                     TongCa = g.Sum(x => (double?)x.TONGCA) ?? 0
+                                 })
+                                 .Take(5)
+                                 .ToList();
+
+            if (model.TopSalaries != null && model.TopSalaries.Any())
+                model.MaxBaseSalary = model.TopSalaries.Max(x => x.Value);
+
+            if (model.TopBonuses != null && model.TopBonuses.Any())
+                model.MaxBonus = model.TopBonuses.Max(x => x.Value);
+
+            if (model.TopPenalties != null && model.TopPenalties.Any())
+                model.MaxPenalty = model.TopPenalties.Max(x => x.Value);
+
+            if (model.TopOvertime != null && model.TopOvertime.Any())
+                model.MaxOvertime = model.TopOvertime.Max(x => x.TongCa);
+
+            model.RecentLogs = db.ACTIVITY_LOG
+                .OrderByDescending(x => x.AcTionTime)
+                .Take(8)
+                .ToList();
+
+            return View(model);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
---
+++ Models/HRDashboardViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QLNSVATC.Models
+{
+    public class HRDashboardViewModel
+    {
+        // Tổng quan
+        public int TotalEmployees { get; set; }
+        public int TotalDepartments { get; set; }
+        public int TotalMale { get; set; }
+        public int TotalFemale { get; set; }
+        public int EmployeesWithInsurance { get; set; }
+        public int EmployeesWithHealthInfo { get; set; }
+        public decimal MaxBaseSalary { get; set; }
+        public decimal MaxBonus { get; set; }
+        public decimal MaxPenalty { get; set; }
+        public double MaxOvertime { get; set; }
+
+        // Top lương / thưởng / phạt
+        public List<EmployeeRankingItem> TopSalaries { get; set; }
+        public List<EmployeeRankingItem> TopBonuses { get; set; }
+        public List<EmployeeRankingItem> TopPenalties { get; set; }
+
+        // Tổng hợp phòng ban
+        public List<DepartmentSummaryItem> DepartmentSummaries { get; set; }
+
+        // Chấm công / tăng ca
+        public List<AttendanceSummaryItem> TopOvertime { get; set; }
+
+        // Log hoạt động gần đây
+        public List<ACTIVITY_LOG> RecentLogs { get; set; }
+
+        public HRDashboardViewModel()
+        {
+            TopSalaries = new List<EmployeeRankingItem>();
+            TopBonuses = new List<EmployeeRankingItem>();
+            TopPenalties = new List<EmployeeRankingItem>();
+            DepartmentSummaries = new List<DepartmentSummaryItem>();
+            TopOvertime = new List<AttendanceSummaryItem>();
+            RecentLogs = new List<ACTIVITY_LOG>();
+        }
+    }
+
+    public class EmployeeRankingItem
+    {
+        public string MaNV { get; set; }
+        public string HoTen { get; set; }
+        public string TenPhongBan { get; set; }
+
+        // Giá trị chính dùng để xếp hạng (lương / thưởng / phạt)
+        public decimal Value { get; set; }
+
+        // Đơn vị hiển thị (VND, giờ,...)
+        public string Unit { get; set; }
+
+        // Nhãn mô tả (Lương cơ bản, Tổng thưởng,...)
+        public string Label { get; set; }
+    }
+
+    public class DepartmentSummaryItem
+    {
+        public string MaPB { get; set; }
+        public string TenPB { get; set; }
+        public int SoNhanVien { get; set; }
+        public int SoNam { get; set; }
+        public int SoNu { get; set; }
+    }
+
+    public class AttendanceSummaryItem
+    {
+        public string MaNV { get; set; }
+        public string HoTen { get; set; }
+        public string TenPhongBan { get; set; }
+        public double TongCa { get; set; } // tổng ca làm việc (TONGCA)
+    }
+}
